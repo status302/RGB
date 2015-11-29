c3d87d746654291012e6e6800a25a775f30df6b3
Qiuncheng <qiuncheng@gmail.com>
2015-11-29 22:21:13 +0800

    Initial Commit

---
+++ RGB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB
+//
+//  Created by 程庆春 on 15/11/29.
+//  Copyright © 2015年 Qiun cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
