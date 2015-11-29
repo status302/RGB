8d4a920bf5a1a5c608d42a2085406f53cd650606
Qiuncheng <qiuncheng@gmail.com>
2015-11-29 22:51:11 +0800

    rbg
    简易RGB动态颜色改变

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var redSlide: UISlider!
+    @IBOutlet weak var greenSlide: UISlider!
+    @IBOutlet weak var blueSlide: UISlider!
+    @IBOutlet weak var alphaSlide: UISlider!
+    @IBOutlet weak var showView: UIView!
+    @IBOutlet weak var redValueLable: UILabel!
+    @IBOutlet weak var greenValueLable: UILabel!
+    @IBOutlet weak var blueValueLable: UILabel!
+    @IBOutlet weak var alphaValueChange: UILabel!
 
+    @IBAction func slideValueChange(sender: AnyObject) {
+        showView.backgroundColor = UIColor(red: CGFloat(redSlide.value), green: CGFloat(greenSlide.value), blue: CGFloat(blueSlide.value), alpha: CGFloat(alphaSlide.value))
+        
+        redValueLable.text = "\(redSlide.value)"
+        greenValueLable.text = "\(greenSlide.value)"
+        blueValueLable.text = "\(blueSlide.value)"
+        alphaValueChange.text = "\(alphaSlide.value)"
+
+        alphaSlide.thumbTintColor = showView.backgroundColor
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        slideValueChange(self)
     }
 
     override func didReceiveMemoryWarning() {
